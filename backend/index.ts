566115bf85ecf0d1c60f180a1a5fccd260fa1617
Amaan Pasha <amaanpasha23@gmail.com>
2026-07-07 03:41:52 +0530

    prompt added and ai is integrated with free credit of 5$

--- backend/index.ts
+++ backend/index.ts
@@ -1,33 +1,44 @@
-import {tavily} from "@tavily/core";
+import { tavily } from "@tavily/core";
+import { streamText } from "ai";
 import express from "express";
+import { PROMPT_TEMPLATE, SYSTEM_PROMPT } from "./prompt";
 
 const client = tavily({ apiKey: process.env.TAVILY_API_KEY });
 
 const app = express();
 app.use(express.json());
 
-app.post("/purplexity_ask", (req, res)=>{
-    //Step-1 get query from the user 
-    const query = req.body.query;
+app.post("/purplexity_ask", async (req, res) => {
+  //Step-1 get query from the user
+  const query = req.body.query; //give the best rust resources
 
-    //Step-2 make sure the user has the access/credits to hit the endpoint.
+  //Step-2 make sure the user has the access/credits to hit the endpoint.
+
+  //Step-3 check we have web search index for the similar query or not...
+
+  //Step-4 if no the do web search to gather the resources.....
+
+  const webSearchResponse = await client.search(query, {
+    searchDepth: "advanced",
+  });
+
+  const webSearchResult = webSearchResponse.results;
+  //Step-5 do some context engineering on the prompt + web search responses...
+
+  //Step-6 hit the llm and stream back the response......
+
+  const prompt = PROMPT_TEMPLATE
+  .replace("{{WEB_SEARCH_RESULTS}}", JSON.stringify(webSearchResult))
+  .replace("{{USER_QUERY}}", query);
 
 
-    //Step-3 check we have web search index for the similar query or not...
+  const result = streamText({
+    model: "openai/gpt-5.4",
+    prompt: prompt,
+    system: SYSTEM_PROMPT
+  });
 
-
-    //Step-4 if no the do web search to gather the resources.....
-
-
-    //Step-5 do some context engineering on the prompt + web search responses...
-
-
-    //Step-6 hit the llm and stream back the response......
-
-
-    //Step-7 also stream back the sources and the follow up questions .....(which we will get from another prallell LLM also)
-
-
-})
+  //Step-7 also stream back the sources and the follow up questions .....(which we will get from another prallell LLM also)
+});
 
 app.listen(3000);
