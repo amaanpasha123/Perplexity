b445d3b1e10dcbe31623d2a6d8489fd3e08b25bc
Amaan Pasha <amaanpasha23@gmail.com>
2026-07-10 02:25:40 +0530

    these file is working well

--- frontend/src/pages/Auth.tsx
+++ frontend/src/pages/Auth.tsx
@@ -1,11 +1,27 @@
-export default function Auth(){
+import { createClient } from "@supabase/supabase-js";
+const supabase = createClient(
+  process.env.VITE_SUPABASE_URL!,
+  process.env.VITE_SUPABASE_PUBLISHABLE_KEY!,
+);
 
-    function login() {
-        
+export default function Auth() {
+  async function login(provider: "github" | "google") {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider: "github",
+    });
+
+    if(error){
+        alert("Some Error is there in this");
+    }else{
+        alert("SingedIn");
     }
-    return <div>
-        <button onClick={()=> login("Google")}>Login with Google</button>
-        <button onClick={()=> login("Github")}>Login with Github</button>
-    </div>
-}
+  }
 
+  
+  return (
+    <div>
+      <button onClick={() => login("Google")}>Login with Google</button>
+      <button onClick={() => login("Github")}>Login with Github</button>
+    </div>
+  );
+}
