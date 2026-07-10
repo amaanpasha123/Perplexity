12273d02cd793de6cad0ea017e167ae1f7767eb5
Amaan Pasha <amaanpasha23@gmail.com>
2026-07-11 02:54:19 +0530

    Just taking the supabase userId over here

--- backend/middleware.ts
+++ backend/middleware.ts
@@ -1,5 +1,11 @@
 import type { NextFunction } from "express";
+import { createSupabaseClient } from "./client";
 
-export function middleware(req:Request, res : Response, next : NextFunction){
-    
+const client = createSupabaseClient();
+
+export function middleware(req: Request, res: Response, next: NextFunction) {
+  const token = (req.headers as any).authorization;
+  // Checking if user is valid or not over here from the supabase
+  client.auth.getUser(token);
+  
 }
